6f97999d8af9dc5452df172d84042547830fc5d8
Zuhaib <zuhaib@nerdpad.com>
2019-03-29 19:08:14 -0400

    correct the errors

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React! <- This was changed
+            Learn React! This was changed
           </a>
         </header>
       </div>
